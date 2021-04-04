b1da69413d68ce5695ae1f38d2ed121858982bd4
Kevin Herrera <kevin@herrera.io>
2021-04-03 22:22:08 -0700

    Using react-bootstrap.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import App from "components/pages/App";
 import ReactDOM from "react-dom";
 import { StrictMode } from "react";
 
+import "styles/global.scss";
+
 ReactDOM.render(
   <StrictMode>
     <App />
