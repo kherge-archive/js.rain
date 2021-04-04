fa1e95c198a99dcee9ec63c8fb8f2af4b66e15e5
Kevin Herrera <kevin@herrera.io>
2021-04-04 11:48:01 -0700

    Fixing system tray handling.

--- public/electron.js
+++ public/electron.js
@@ -1,19 +1,23 @@
 const isDev = require("electron-is-dev");
 const path = require("path");
 const { BrowserWindow, Menu, Tray, app, nativeImage } = require("electron");
+const { none, some } = require("@kherge/result");
+
+// The path to the icon.
+const icon = path.join(__dirname, "favicon.ico");
 
 // Integrate Windows support.
 if (require("electron-squirrel-startup")) {
   app.quit();
 }
 
+// Create a global system tray.
+let tray;
+
 /**
  * Creates and configures a new browser window.
  */
 const createWindow = () => {
-  // Get the path to the icon.
-  const icon = path.join(__dirname, "favicon.ico");
-
   // Create a new window.
   const window = new BrowserWindow({
     icon,
@@ -33,12 +37,6 @@ const createWindow = () => {
     window.setSize(preferredSize.width, preferredSize.height);
   });
 
-  // Hide the window when closed.
-  window.on("close", (event) => {
-    event.preventDefault();
-    window.hide();
-  });
-
   // Hide the window when minimized.
   window.on("minimize", (event) => {
     event.preventDefault();
@@ -51,27 +49,6 @@ const createWindow = () => {
   // Disable resizing.
   window.setResizable(false);
 
-  // Set a context menu to show the window or quit.
-  const tray = new Tray(nativeImage.createFromPath(icon));
-
-  tray.setToolTip("Rain");
-  tray.setContextMenu(
-    Menu.buildFromTemplate([
-      {
-        label: "Show",
-        click: () => {
-          window.show();
-        },
-      },
-      {
-        label: "Quit",
-        click: () => {
-          app.quit();
-        },
-      },
-    ])
-  );
-
   // Load the React app.
   if (isDev) {
     window.loadURL("http://localhost:3000");
@@ -81,19 +58,39 @@ const createWindow = () => {
   }
 };
 
-// When the macOS dock app icon is clicked, re-open the window.
-app.on("activate", () => {
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
+/**
+ * Returns the available window.
+ *
+ * @return The window, if any.
+ */
+const getWindow = () => {
+  const windows = BrowserWindow.getAllWindows();
 
-// Do not exist if all windows are closed on macOS.
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
-    app.quit();
+  if (windows.length > 0) {
+    return some(windows[0]);
   }
-});
+
+  return none();
+};
 
 // Create the window when the browser is ready.
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  tray = new Tray(nativeImage.createFromPath(icon));
+
+  tray.on("click", () => getWindow().map((window) => window.show()));
+  tray.setToolTip("Rain");
+  tray.setContextMenu(
+    Menu.buildFromTemplate([
+      {
+        label: "Show",
+        click: () => getWindow().map((window) => window.show()),
+      },
+      {
+        label: "Quit",
+        click: () => app.quit(),
+      },
+    ])
+  );
+
+  createWindow();
+});
