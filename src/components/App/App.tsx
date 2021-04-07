f4f72861113595e89910142a7c3c2ca6ee8c55a4
Kevin Herrera <kevin@herrera.io>
2021-04-07 15:37:01 -0700

    Supporting dark mode.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,17 @@ import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import Sound from "../Sound";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
+import { ColorScheme, getColorScheme } from "@kherge/prefers-color-scheme";
+import {
+  ThemeProvider,
+  createMuiTheme,
+  makeStyles,
+} from "@material-ui/core/styles";
 
 import "./App.css";
+import { useMemo } from "react";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   title: {
     fontSize: "3rem",
     marginBottom: "0.4rem",
@@ -16,28 +22,40 @@ const useStyles = makeStyles((theme) => ({
 const App = () => {
   console.debug(`<App/>`);
 
+  const scheme = getColorScheme();
   const styles = useStyles();
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: scheme,
+        },
+      }),
+    [scheme]
+  );
 
   return (
-    <Container disableGutters={true}>
-      <Grid container>
-        <Grid item xs={12}>
-          <Typography
-            align="center"
-            className={styles.title}
-            component="h1"
-            variant="h1"
-          >
-            Rain
-          </Typography>
+    <ThemeProvider theme={theme}>
+      <Container disableGutters={true}>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography
+              align="center"
+              className={styles.title}
+              component="h1"
+              variant="h1"
+            >
+              Rain
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
-      <Sound label="Rain" url="static/mp3/rain.mp3" />
-      <Sound label="Splashing" url="static/mp3/splashing.mp3" />
-      <Sound label="Storm" url="static/mp3/storm.mp3" />
-      <Sound label="Thunder" url="static/mp3/thunder.mp3" />
-      <Sound label="Window" url="static/mp3/window.mp3" />
-    </Container>
+        <Sound label="Rain" url="static/mp3/rain.mp3" />
+        <Sound label="Splashing" url="static/mp3/splashing.mp3" />
+        <Sound label="Storm" url="static/mp3/storm.mp3" />
+        <Sound label="Thunder" url="static/mp3/thunder.mp3" />
+        <Sound label="Window" url="static/mp3/window.mp3" />
+      </Container>
+    </ThemeProvider>
   );
 };
 
